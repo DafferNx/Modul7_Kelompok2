33e150b3ee0a0d504596e24298c71e5eec43ff4c
ranggaluthfiendi <echo.rangrang@gmail.com>
2026-04-18 14:15:24 +0700

    menambahkan class FilmFavorit_103022400021.cs

---
+++ Modul7_Kelompok2/FilmFavorit_103022400021.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.Json;
+
+public class FilmFavorit
+{
+	public string title { get; set; }
+	public string director { get; set; }
+	public string year { get; set; }
+	public string genre { get; set; }
+	public int rating { get; set; }
+	public int durationMinutes { get; set; }
+	public bool isWatched { get; set; }
+}
+
+public class FilmFavorit_103022400021
+{
+	public void ReadJSON()
+	{
+		string json = File.ReadAllText(@"D:\Modul7_Kelompok2\Modul7_Kelompok2/jurnal7_1_103022400021.json");
+
+		FilmFavorit fav = JsonSerializer.Deserialize<FilmFavorit>(json);
+
+		Console.WriteLine($"Title: {fav.title}, Director: {fav.director}, Year: {fav.year}, Genre: {fav.genre}, Rating: {fav.rating}, Duration Minutes: {fav.durationMinutes}, Is Watched: {fav.isWatched}");
+	}
+}
