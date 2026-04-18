f13d5ce8c51497cae5b638dc89d44682f5263b53
DafferNx <filbertdeven20@gmail.com>
2026-04-18 15:06:27 +0700

    menambahkan class GenreDictionary_103022400112

--- Modul7_Kelompok2/Program.cs
+++ Modul7_Kelompok2/Program.cs
@@ -1,11 +1,13 @@
 ﻿class Modul7_Kelompok2
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
         FilmFavorit_103022400112 filmFavorit = new FilmFavorit_103022400112();
         filmFavorit.readJSON();
         Console.WriteLine();
         WatchList_103022400112 watchList = new WatchList_103022400112();
         watchList.readJSON();
+        GenreDictionary_103022400112 genreDictionary = new GenreDictionary_103022400112();
+        genreDictionary.readJSON();
     }
 }
