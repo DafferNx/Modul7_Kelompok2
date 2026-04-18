1c7e89755e4206ff492a0ce89d05088910ea6e01
unknown <praktikan@iflab.com>
2026-04-18 14:25:06 +0700

    menambahkan class FilmFavorit_103022400027_fadhil

---
+++ Modul7_Kelompok2/FilmFavorit_103022400027_fadhil.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+public class Film
+{
+    public string title { get; set; }
+    public string director { get; set; }
+    public string year { get; set; }
+    public string genre { get; set; }
+    public int rating { get; set; }
+    public int durationMinutes { get; set; }
+    public Boolean isWatched { get; set; }
+}
+public class FilmFavorit_103022400027_fadhil
+{
+	
+    public FilmFavorit_103022400027_fadhil()
+	{
+
+	}
+	public void ReadJSON()
+	{
+		string JSON = File.ReadAllText(@"D:\Modul7_Kelompok2\Modul7_Kelompok2\jurnal7_1_103022400027.json");
+		Film film = JsonSerializer.Deserialize<Film>(JSON);
+		Console.WriteLine("Judul: " + film.title);
+        Console.WriteLine("Director: " + film.director);
+        Console.WriteLine("Year: " + film.year);
+        Console.WriteLine("Genre: " + film.genre);
+        Console.WriteLine("Rating: " + film.rating);
+        Console.WriteLine("Durasi: " + film.durationMinutes);
+        Console.WriteLine("Watched?: " + film.isWatched);
+    }
+}
--- Modul7_Kelompok2/Program.cs
+++ Modul7_Kelompok2/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿FilmFavorit_103022400027_fadhil film = new FilmFavorit_103022400027_fadhil();
+film.ReadJSON();
