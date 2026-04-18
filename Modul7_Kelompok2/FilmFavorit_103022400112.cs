b223c93f0e78b5b479502f2d830acf0417244d9c
DafferNx <filbertdeven20@gmail.com>
2026-04-18 14:15:19 +0700

    menambahkan class FilmFavorit_103022400112

---
+++ Modul7_Kelompok2/FilmFavorit_103022400112.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.Json;
+
+public class Film
+{
+    public string title { get; set; }
+    public string director { get; set; }
+    public string year { get; set; }
+    public string genre { get; set; }
+    public int rating { get; set; }
+    public int durationMinutes { get; set; }
+    public bool isWatched { get; set; }
+}
+
+public class FilmFavorit_103022400112
+{
+
+    public void readJSON()
+    {
+        string jsonFilePath = "D:/Modul7_Kelompok2/Modul7_Kelompok2/bin/Debug/net10.0/jurnal7_1_103022400112.json";
+        string jsonString = File.ReadAllText(jsonFilePath);
+        Film film = JsonSerializer.Deserialize<Film>(jsonString);
+        Console.WriteLine("Title: " + film.title);
+        Console.WriteLine("Director: " + film.director);
+        Console.WriteLine("Year: " + film.year);
+        Console.WriteLine("Genre: " + film.genre);
+        Console.WriteLine("Rating: " + film.rating);
+        Console.WriteLine("Duration (minutes): " + film.durationMinutes);
+        Console.WriteLine("Watched: " + (film.isWatched ? "Yes" : "No"));
+    }
+}
--- Modul7_Kelompok2/Program.cs
+++ Modul7_Kelompok2/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Modul7_Kelompok2
+{
+    static void Main(string[] args)
+    {
+        FilmFavorit_103022400112 filmFavorit = new FilmFavorit_103022400112();
+        filmFavorit.readJSON();
+    }
+}
